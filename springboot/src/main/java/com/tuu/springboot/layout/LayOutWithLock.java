8b68e49eea87c584e72357a30e11fb148050902e
tuu <nba5183tzg@163.com>
2020-09-08 16:07:45 +0800

    jvm 参数

--- springboot/src/main/java/com/tuu/springboot/layout/LayOutWithLock.java
+++ springboot/src/main/java/com/tuu/springboot/layout/LayOutWithLock.java
@@ -3,8 +3,12 @@ package com.tuu.springboot.layout;
 
 import org.openjdk.jol.info.ClassLayout;
 
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
 public class LayOutWithLock {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        Thread.sleep(5*1000);
         Object object = new Object();
         synchronized (object) {
             System.out.println(ClassLayout.parseInstance(object).toPrintable());
---
+++ springboot/src/main/java/com/tuu/springboot/test/Test.java
@@ -0,0 +1,8 @@
+package com.tuu.springboot.test;
+
+public class Test {
+    public static void main(String[] args) {
+        String s = "s";
+        System.out.println(Byte.MAX_VALUE);
+    }
+}
