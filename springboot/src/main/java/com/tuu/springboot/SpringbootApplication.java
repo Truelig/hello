bc7d3d4e5c5bae075035d28a76c0f31b92546ea2
tuu <nba5183tzg@163.com>
2020-09-09 15:54:33 +0800

    解析yml

--- springboot/src/main/java/com/tuu/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/tuu/springboot/SpringbootApplication.java
@@ -2,9 +2,12 @@ package com.tuu.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+//判断容器有没有摸个类，如果有就注入当前类
+//@ConditionalOnClass
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @ComponentScan("com")
 public class SpringbootApplication {
--- springboot/src/test/java/com/tuu/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/tuu/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,31 @@
 package com.tuu.springboot;
 
+import com.tuu.springboot.to.Student;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@SpringBootTest
+import javax.annotation.security.RunAs;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 class SpringbootApplicationTests {
 
-//    @Test
-    void contextLoads() {
+    @Value("${a}")
+    String a;
+    @Value("#{${b}+${c}}")
+    int sum;
+    @Autowired
+    Student student;
+
+    @Test
+    void test() {
+        System.out.println(a);
+        System.out.println(sum);
+        System.out.println(student);
     }
 
 }
