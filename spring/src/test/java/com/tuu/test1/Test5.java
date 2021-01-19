05d6ee0cd68844220c17a60eb7c403db85c77c85
tuzhenguang <tuzhenguang@danmi.com>
2021-01-19 10:23:26 +0800

    3

---
+++ spring/src/test/java/com/tuu/test1/Test5.java
@@ -0,0 +1,20 @@
+package com.tuu.test1;
+
+import org.junit.Test;
+
+public class Test5 {
+
+    @Test
+    public void test1() {
+        //YYYY 所在天周日开始，周六结束，如果跨年，则年份为下一年
+//        SimpleDateFormat df1 = new SimpleDateFormat("YYYY-MM");
+//        SimpleDateFormat df2 = new SimpleDateFormat("yyyy-MM-dd");
+//        Date date1 = new Date();
+//        System.out.println("YYYY-MM-dd-----"+df1.format(date1));
+//        System.out.println("yyyy-MM-dd-----"+df2.format(date1));
+        Long l1 = 0L;
+        Long l2 = 3L;
+
+        System.out.println(1d*l1 );
+    }
+}
