995658721bfc16babf668c98679f33bcac3768d6
tuu <523104286@qq.com>
2021-03-15 22:57:21 +0800

    ceshi

--- spring/src/main/java/com/tuu/util/ListUtil.java
+++
@@ -1,9 +0,0 @@
-package com.tuu.util;
-
-import java.util.List;
-
-public class ListUtil {
-    public static List<String> getStringList(){
-        return List.of("java", "c++", "python", "go", "js", "vue", "oracle", "mysql");
-    }
-}
